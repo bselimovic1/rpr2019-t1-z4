47a543fb971cab6fa72d3bd8277f384fae61e526
Belmin Selimovic <bselimovic1@etf.unsa.ba>
2019-10-17 16:43:46 +0200

    Popravljene funkcije

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl artikli[] = new Artikl[50];
+
+    public Korpa(){}
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        Artikl izbaceni=new Artikl();
+        for(int i=0;i<artikli.length;i++){
+            if(artikli[i].getKod().equals(kod)){
+                izbaceni=artikli[i];
+                artikli[i]=null;
+            }
+        }
+        return izbaceni;
+    }
+    public boolean dodajArtikl(Artikl art){
+        boolean dodat= false;
+        for(int i=0;i<artikli.length;i++){
+            if(artikli[i]==null){
+                artikli[i]=art;
+                return true;
+            }
+        }
+        return dodat;
+    }
+    public int dajUkupnuCijenuArtikala(){
+        int ukupnaCijena=0;
+        for(int i=0;i<artikli.length;i++){
+            ukupnaCijena+=artikli[i].getCijena();
+        }
+        return ukupnaCijena;
+    }
 }
